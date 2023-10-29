8b5f1f2804c6c4867bd2f311b33b4c8a73360150
Mayank Kapoor <mayankkapoormail@gmail.com>
2023-10-29 19:01:40 +0530

    auto format changes (#5)

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+import os
+import logging
+import sys
 from flask import request, jsonify
 import openai
 import psycopg2
-import os
 from psycopg2.extras import DictCursor
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
-import logging
-import sys
 from models import app
 
 # Initialize OpenAI API
-openai.api_key = os.environ.get('OPENAI_API_KEY')  # This could also be an environment variable
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Add logging
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # check if storage already exists
-if (not os.path.exists('./storage')):
+if not os.path.exists('./storage'):
     # load the documents and create the index
     documents = SimpleDirectoryReader('./data').load_data()
     index = VectorStoreIndex.from_documents(documents)
@@ -27,34 +27,65 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir='./storage')
     index = load_index_from_storage(storage_context)
 
+
 @app.route('/', methods=['GET'])
 def health_check():
+    """
+    Checks the health of the application's database connection.
+
+    Returns:
+        A JSON response indicating the status of the health check.
+    """
     try:
         # Try to connect to the database
         connection = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
         cursor = connection.cursor()
-        cursor.execute('SELECT 1')  # Simple query to check the database connection
+        # Simple query to check the database connection
+        cursor.execute('SELECT 1')
         cursor.close()
         connection.close()
-        
+
         return jsonify({"status": "Healthy", "database": "Connected"}), 200
     except Exception as e:
         return jsonify({"status": "Unhealthy", "error": str(e)}), 500
 
+
 def save_question_and_answer(question, answer):
+    """
+    Save a question and its corresponding answer to the database.
+
+    Args:
+        question (str): The question to be saved.
+        answer (str): The answer to the question.
+
+    Returns:
+        None
+    """
     # Connect to the database
     connection = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
     cursor = connection.cursor(cursor_factory=DictCursor)
 
     # Save data to database
-    cursor.execute('INSERT INTO questions (question, answer) VALUES (%s, %s)', (question, answer))
+    cursor.execute(
+        'INSERT INTO questions (question, answer) VALUES (%s, %s)', (question, answer))
     connection.commit()
 
     cursor.close()
     connection.close()
 
+
 @app.route('/ask', methods=['POST'])
 def ask():
+    """
+    Process a question from the user and return an answer.
+
+    Parameters:
+    - question
+
+    Returns:
+    - The answer to the user's question.
+
+    """
     question = request.json.get('question', '')
 
     query_engine = index.as_query_engine()
@@ -66,6 +97,7 @@ def ask():
     save_question_and_answer(question, answer)
 
     return jsonify({"answer": answer})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
